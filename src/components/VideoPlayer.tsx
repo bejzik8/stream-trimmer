2f1d98d3bc28e8f412f8e838870482e45a4f5970
Basic Mirko <bejzik8@gmail.com>
2022-06-17 03:24:38 +0100

    Created Controls component, added playPauseVideo and stopVideo functionalities to the player.

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,13 +1,52 @@
+import { useRef, useEffect } from 'react'
 import styled from 'styled-components'
+
+import Button from './Button'
 
 type VideoPlayerProps = {
     source: string
 }
 
-const VideoPlayer = ({ source }: VideoPlayerProps) =>
-    <StyledVideo controls>
-        <source src={source} />
-    </StyledVideo>
+const VideoPlayer = ({ source }: VideoPlayerProps) => {
+    const videoPlayer = useRef<null | HTMLMediaElement>(null)
+
+    const playPauseVideo = () => {
+        if (!videoPlayer.current) return
+
+        videoPlayer.current.paused ? videoPlayer.current.play() : videoPlayer.current.pause()
+    }
+
+    const stopVideo = () => {
+        if (!videoPlayer.current) return
+
+        videoPlayer.current.pause()
+        videoPlayer.current.currentTime = 0
+    }
+
+    return <>
+        <StyledVideo controls ref={videoPlayer}>
+            <source src={source} />
+        </StyledVideo>
+        <ControlsContainer>
+            <Button
+                text='P'
+                onClick={playPauseVideo}
+            />
+            <Button
+                text='S'
+                onClick={stopVideo}
+            />
+        </ControlsContainer>
+    </>
+}    
+
+const ControlsContainer = styled.div`
+    display: inline-block;
+    width: 60%;
+    height: 32px;
+    background-color: rgba(0, 0, 0, 0.3);
+    margin-top: 10px;
+`
 
 const StyledVideo = styled.video`
     width: 60%;
