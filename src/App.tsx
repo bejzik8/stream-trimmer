94061122f77819946670b4d7ddbfde339abfb0b6
Basic Mirko <bejzik8@gmail.com>
2022-06-17 02:36:06 +0100

    VideoPlayer component imported, updatred tsconfig.json file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import styled from 'styled-components'
+
+import VideoPlayer from './components/VideoPlayer'
+
+import video from './assets/file_example_MP4_1920_18MG.mp4'
+
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <VideoPlayer source={video} />
+    </Container>
   )
 }
+
+const Container = styled.div`
+  width: 100%;
+  text-align: center;
+  padding-top: 50px;
+`
 
 export default App
